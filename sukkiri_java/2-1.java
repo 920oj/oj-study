af70eff5880929a1684fba32e5d988be5161d2a7
Junya Oiwa <0920oiwa@gmail.com>
2022-07-04 23:36:59 +0900

    [add]2-1

---
+++ sukkiri_java/2-1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int x = 5;
+        int y = 10;
+        String ans = "x+yは" + (x + y); // 優先順位を変更
+        System.out.println(ans);
+    }
+}
