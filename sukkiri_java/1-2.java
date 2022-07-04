6311b5b429986ee3490408fe7a69bd6a2af7651b
Junya Oiwa <0920oiwa@gmail.com>
2022-07-04 23:31:30 +0900

    [add]1-2

---
+++ sukkiri_java/1-2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int vertical = 3;
+        int width = 5;
+        int result = width * vertical;
+        System.out.println("縦幅" + vertical + "横幅" + width + "の長方形の面積は、" + result);
+    }
+}
