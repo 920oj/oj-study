af72c56c0801f175604c7f14662a0711eb9711a1
Junya Oiwa <0920oiwa@gmail.com>
2022-07-04 23:31:40 +0900

    [add]1-3

---
+++ sukkiri_java/1-3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        boolean result = true;
+        char favKanji = '駆';  // char型（1文字）の場合はシングルクォートを使う（リテラル）
+        double pi = 3.14; // floatでもOK、doubleのほうがより厳密な計算を行うことができる
+        long piNumber = 314159265358979L; // Lのリテラルはlongの意
+        String attackMessage = "ミナトの攻撃！敵に15ポイントのダメージを与えた。"; // string型（文字列）の場合はダブルクォートを使う（リテラル）
+    }
+}
